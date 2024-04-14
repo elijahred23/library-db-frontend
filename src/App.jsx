ec0027fc7d6c13d093c875c23b564426ff78549b
elijahred23 <elijahred23@gmail.com>
2024-04-13 21:35:39 -0500

    Eli-3: Added material ui styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Books from './Books';
 import Users from './Users';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
-function App() {
+export default function App() {
   return (
     <Router>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h5" component="div" sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
+            LIBRARY DB
+          </Typography>
+          <Link className="link" to="/" color="inherit" underline="none">
+            Home
+          </Link>
+          <Link className="link" to="/books" color="inherit" underline="none">
+            Books
+          </Link>
+          <Link className="link" to="/users" color="inherit" underline="none">
+            Users
+          </Link>
+        </Toolbar>
+      </AppBar>
       <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/books">Books</Link>
-          <Link to="/users">Users</Link>
-        </nav>
         <Routes>
-          <Route path="/" element={<div>LIBRARY DB</div>} />
+          <Route path="/" element={<div style={{display:"flex",marginTop:"20vh",alignItems:"center", justifyContent:"center"}}><div>Home</div></div>} />
           <Route path="/books" element={<Books />} />
           <Route path="/users" element={<Users />} />
         </Routes>
@@ -22,4 +34,3 @@ function App() {
     </Router>
   );
 }
-export default App;
